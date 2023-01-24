1a3db9688fe071e406aadba0011b34b354272063
Fidanmus <fidan.mus.88@gmail.comm>
2023-01-23 19:53:55 -0700

    adding changes

--- src/App.js
+++ src/App.js
@@ -5,23 +5,29 @@ import MyRecipesComponent from './MyRecipesComponent';
 
 function App() {
 
-  const [mySearch, setMySearch] = useState("");
-  const [myRecipes, setMyRecipes] = useState([])
+  const [mySearch, setMySearch] = useState('');
+  const [myRecipes, setMyRecipes] = useState([]);
+  const [wordSubmitted, setWordSubmitted] = useState('')
 
   useEffect ( () => {
     getRecipe()
-    }, [])
+    }, [wordSubmitted])
 
     const getRecipe = async () => {
-    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=374446f2&app_key=8e9798184625b3729bc2356a86702480`);
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=374446f2&app_key=8e9798184625b3729bc2356a86702480`);
     const data = await response.json();
-    console.log(data)
-    setMyRecipes(data);
+    console.log(data.hits)
+    setMyRecipes(data.hits);
   }
   
   const myRecipeSearch = (e) => {
     console.log(e.target.value)
     setMySearch(e.target.value)
+  }
+
+  const finalSearch = (e) => {
+    e.preventDefault();
+    setWordSubmitted(mySearch)
   }
 
   return (
@@ -34,12 +40,14 @@ function App() {
       </div>
 
       <div className="container">
+        <form onSubmit={finalSearch}>
         <input className="search" placeholder="Search..." onChange={myRecipeSearch} value={mySearch}></input>
+        </form>
       </div>
 
       <div className="container">
         <button>
-          <img src="https://img.icons8.com/external-flatart-icons-flat-flatarticons/512/external-pan-home-appliances-and-kitchen-flatart-icons-flat-flatarticons.png" className='icons' width="50px" height="50px" />
+          <img src="https://img.icons8.com/external-flatart-icons-flat-flatarticons/512/external-pan-home-appliances-and-kitchen-flatart-icons-flat-flatarticons.png" className='icons' width="50px" height="50px" alt="pan"/>
         </button>
       </div>
 
@@ -48,6 +56,9 @@ function App() {
         <MyRecipesComponent 
         label={element.recipe.label} 
         image={element.recipe.image}
+        calories={element.recipe.calories}
+        ingredients={element.recipe.ingredientLines}
+        link={element.recipe.url}
         />
       
       ))}
--- src/MyRecipesComponent.js
+++ src/MyRecipesComponent.js
@@ -1,7 +1,26 @@
-function MyRecipesComponent ({label, image}) {
+function MyRecipesComponent ({label, image, calories, ingredients, link}) {
     return (<div>
+        <div className="container">
         <h2>{label}</h2>
-        <img src={image}></img>
+        </div>
+        
+        <div className="container">
+            <a href={link}>
+        <img className="tasty" src={image} alt="recipeImage"></img>
+        </a>
+        </div>
+
+        <ul className="list">
+        {ingredients.map(ingredient => (
+            <li><img src="https://img.icons8.com/fluency/512/ok.png" className="icon" alt="tickmark"/>
+                {ingredient}</li>
+        ))}
+        </ul>
+        
+        <div className="container">
+        <p className="par">{calories.toFixed()} calories</p>
+        </div>
+        
     </div>
     )
 }
